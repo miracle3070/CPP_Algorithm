795821ca16e4ecb894b5eac16d8f61b0ba4f0e5f
miracle3070 <miracle3070@naver.com>
2023-06-17 18:37:09 +0900

    [sec01] 5. 나이 계산

---
+++ Section 01/5. 나이 계산.cpp
@@ -0,0 +1,23 @@
+﻿#pragma warning(disable:4996)
+#include <stdio.h>
+using namespace std;
+
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	char id[20];
+	int year;
+	scanf("%s", id);
+
+	if (id[7] == '1' || id[7] == '2')
+		year = 1900 + (id[0] - '0') * 10 + (id[1] - '0');
+	else
+		year = 2000 + (id[0] - '0') * 10 + (id[1] - '0');
+	printf("%d ", (2019 - year + 1));
+
+	if (id[7] == '1' || id[7] == '3')
+		printf("M\n");
+	else
+		printf("W\n");
+
+	return 0;
+}

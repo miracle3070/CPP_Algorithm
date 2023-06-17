28c8fddb9ec055e9f296c276c742ace25a2cc14e
miracle3070 <miracle3070@naver.com>
2023-06-17 14:28:34 +0900

    [sec01] 3. 진약수의 합

---
+++ Section 01/3. 진약수의 합.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, sum = 1;
+	int min = (int)(1e9);
+	bool flag = true;
+	cin >> n;
+	cout << "1 + ";
+
+	for (int i = 2; i < min; i++) {
+		if (n % i == 0) {
+			if (flag) {
+				min = n / i;
+				flag = false;
+			}
+			cout << i << " + ";
+			sum += i;
+		}
+	}
+	sum += min;
+	cout << min << " = " << sum << endl;
+
+	return 0;
+}

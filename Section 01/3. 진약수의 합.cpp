0c755873606203c2c45a19041d469f0704eabae6
miracle3070 <miracle3070@naver.com>
2023-06-17 14:37:10 +0900

    [오류수정][sec-1] 3. 진약수의 합

--- Section 01/3. 진약수의 합.cpp
+++ Section 01/3. 진약수의 합.cpp
@@ -4,23 +4,16 @@ using namespace std;
 int main()
 {
 	int n, sum = 1;
-	int min = (int)(1e9);
-	bool flag = true;
 	cin >> n;
-	cout << "1 + ";
+	cout << "1";
 
-	for (int i = 2; i < min; i++) {
+	for (int i = 2; i < n; i++) {
 		if (n % i == 0) {
-			if (flag) {
-				min = n / i;
-				flag = false;
-			}
-			cout << i << " + ";
+			cout << " + " << i;
 			sum += i;
 		}
 	}
-	sum += min;
-	cout << min << " = " << sum << endl;
+	cout << " = " << sum << endl;
 
 	return 0;
 }

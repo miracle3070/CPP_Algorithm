d6f51cd51c7b0a969622041b263650a79b114776
miracle3070 <miracle3070@naver.com>
2023-06-17 16:53:54 +0900

    [sec01] 4. 나이 차이

---
+++ Section 01/4. 나이 차이.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n, num;
+	int max = 0, min = (int)(1e9);
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		max = (num > max) ? num : max;
+		min = (num < min) ? num : min;
+	}
+
+	cout << max - min;
+	return 0;
+}

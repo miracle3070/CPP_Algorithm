fcd915de01fcb2684ca71f4ec207d5207373c5a9
miracle3070 <miracle3070@naver.com>
2023-06-19 20:31:08 +0900

    [sec01] 8. 올바른 괄호

---
+++ Section 01/8. 올바른 괄호.cpp
@@ -0,0 +1,33 @@
+﻿// 풀이시간: 00:06:28
+
+#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	int open_cnt = 0;
+	char str[50];
+	bool result = true;
+	scanf("%s", str);
+
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] == '(') {
+			open_cnt++;
+		}
+		else {
+			if (open_cnt <= 0) {
+				result = false;
+				break;
+			}
+			open_cnt--;
+		}
+	}
+
+	if (result && open_cnt == 0)
+		printf("YES\n");
+	else
+		printf("NO\n");
+
+	return 0;
+}

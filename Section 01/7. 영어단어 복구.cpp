bc5d453c403deb7542ae2192d8d88de009822a99
miracle3070 <miracle3070@naver.com>
2023-06-19 20:04:16 +0900

    [sec01] 7. 영어단어 복구

---
+++ Section 01/7. 영어단어 복구.cpp
@@ -0,0 +1,28 @@
+﻿// 풀이시간: 
+
+#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	const int DIFF = 'a' - 'A';
+	char str[102] = {0,};
+	char result[102];
+	fgets(str, sizeof(str) / sizeof(char), stdin);
+
+	int idx = 0;
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] == ' ')
+			continue;
+		else if ('A' <= str[i] && str[i] <= 'Z')
+			result[idx++] = str[i] + DIFF;
+		else if ('a' <= str[i] && str[i] <= 'z')
+			result[idx++] = str[i];
+		else {}
+	}
+	result[idx] = '\0';
+
+	printf("%s\n", result);
+	return 0;
+}

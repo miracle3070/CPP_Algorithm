96b38ea19b81d72571ae82525fef600c4a2646ca
miracle3070 <miracle3070@naver.com>
2023-06-24 20:34:02 +0900

    [sec01] 12. 숫자의 총 개수(large)

---
+++ Section 01/12. 숫자의 총 개수(large).cpp
@@ -0,0 +1,29 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	int n;
+	scanf("%d", &n);
+
+	int cnt = 0; // 자릿수
+	int target = n;
+	int result;
+	while (target > 0) {
+		cnt++;
+		target /= 10;
+	}
+
+	result = (n - (int)(pow(10, cnt - 1)) + 1) * cnt;
+	cnt--;
+
+	while (cnt > 0) {
+		result += (int)(pow(10, cnt - 1)) * 9 * cnt;
+		cnt--;
+	}
+
+	printf("%d\n", result);
+	return 0;
+}

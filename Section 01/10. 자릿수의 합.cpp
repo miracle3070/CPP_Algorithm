445f60cfca0e11ab962237d930af9e725f006ab7
miracle3070 <miracle3070@naver.com>
2023-06-24 12:55:44 +0900

    [sec01] 10. 자릿수의 합

---
+++ Section 01/10. 자릿수의 합.cpp
@@ -0,0 +1,40 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+
+int result = 0;
+int max_sum = 0;
+int digit_sum(int x);
+
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	int n;
+	scanf("%d", &n);
+
+	int num, sum;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &num);
+		sum = digit_sum(num);
+
+		if (max_sum < sum) {
+			max_sum = sum;
+			result = num;
+		}
+
+		if (max_sum == sum)
+			result = (num > result) ? num : result;
+	}
+
+	printf("%d\n", result);
+	return 0;
+}
+
+int digit_sum(int x) {
+	int sum = 0;
+	while (x > 0) {
+		sum += (x % 10);
+		x /= 10;
+	}
+
+	return sum;
+}

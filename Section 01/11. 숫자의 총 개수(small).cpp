cab6bb1bb8fe0ee6ae0b1da0bcebaa3f3545a673
miracle3070 <miracle3070@naver.com>
2023-06-24 13:23:25 +0900

    [sec01] 11. 숫자의 총 개수(small)

---
+++ Section 01/11. 숫자의 총 개수(small).cpp
@@ -0,0 +1,21 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+
+int result = 0;
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		int target = i;
+		while (target > 0) {
+			result++;
+			target /= 10;
+		}
+	}
+
+	printf("%d\n", result);
+	return 0;
+}

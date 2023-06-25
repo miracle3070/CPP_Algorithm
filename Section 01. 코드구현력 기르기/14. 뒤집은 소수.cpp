06305d32f009fb44a8893544c79574ae48bc15ec
miracle3070 <miracle3070@naver.com>
2023-06-25 16:38:04 +0900

    [sec01] 14. 뒤집은 소수

---
+++ Section 01. 코드구현력 기르기/14. 뒤집은 소수.cpp
@@ -0,0 +1,52 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+
+int reverse(int x);
+bool isPrime(int x);
+
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		int target;
+		scanf("%d", &target);
+
+		int reversedNum = reverse(target);
+		bool result = isPrime(reversedNum);
+		if (result) {
+			printf("%d ", reversedNum);
+		}
+	}
+
+	printf("\n");
+	return 0;
+}
+
+int reverse(int x) {
+	int reversedNum = 0;
+	while (x > 0) {
+		reversedNum = (reversedNum * 10) + (x % 10);
+		x /= 10;
+	}
+	return reversedNum;
+}
+
+bool isPrime(int x) {
+	if (x == 1)
+		return false;
+
+	bool result = true;
+	int sqrtNum = (int)sqrt(x) + 1;
+	for (int i = 2; i <= sqrtNum; i++) {
+		if (x == i)
+			continue;
+		if (x % i == 0) {
+			result = false;
+			break;
+		}
+	}
+	return result;
+}

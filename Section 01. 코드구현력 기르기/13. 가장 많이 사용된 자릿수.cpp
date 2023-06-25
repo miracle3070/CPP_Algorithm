df18d29359a0e3d8c46b650b944a341bf9e33ab3
miracle3070 <miracle3070@naver.com>
2023-06-25 15:54:57 +0900

    [sec01] 13. 가장 많이 사용된 자릿수

---
+++ Section 01. 코드구현력 기르기/13. 가장 많이 사용된 자릿수.cpp
@@ -0,0 +1,26 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+
+int cnt[10];
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	char numStr[101];
+	scanf("%s", numStr);
+
+	for (int i = 0; numStr[i] != '\0'; i++) {
+		cnt[numStr[i] - '0']++;
+	}
+
+	int result;
+	int maxCnt = 0;
+	for (int i = 0; i < 10; i++) {
+		if (maxCnt <= cnt[i]) {
+			maxCnt = cnt[i];
+			result = i;
+		}
+	}
+	
+	printf("%d\n", result);
+	return 0;
+}

1ff23671a153e00b01310e196380570cc370626b
miracle3070 <miracle3070@naver.com>
2023-06-29 17:19:54 +0900

    [sec01] 19. 분노 유발자

---
+++ Section 01. 코드구현력 기르기/19. 분노 유발자.cpp
@@ -0,0 +1,28 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+
+int st[101];
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	int n;
+	int result = 0;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		int student;
+		scanf("%d", &student);
+		st[i] = student;
+	}
+
+	int max = st[n];
+	for (int i = n - 1; i > 0; i--) {
+		if (max < st[i]) {
+			max = st[i];
+			result++;
+		}
+	}
+
+	printf("%d\n", result);
+	return 0;
+}

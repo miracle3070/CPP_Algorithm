f24addb288a55b385397e7a54e48bdfa08d8ed1a
miracle3070 <miracle3070@naver.com>
2023-06-29 16:43:52 +0900

    [sec01] 17. 선생님 퀴즈

---
+++ Section 01. 코드구현력 기르기/17. 선생님 퀴즈.cpp
@@ -0,0 +1,22 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		int end, answer;
+		scanf("%d %d", &end, &answer);
+		
+		int target = end * (end + 1) / 2;
+		if (answer == target)
+			printf("YES\n");
+		else
+			printf("NO\n");
+	}
+
+	return 0;
+}

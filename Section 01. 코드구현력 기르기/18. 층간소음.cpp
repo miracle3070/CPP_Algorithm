64958f02c679263c7a9640a45db85e89b68bd22a
miracle3070 <miracle3070@naver.com>
2023-06-29 16:55:37 +0900

    [sec01] 18. 층간소음

---
+++ Section 01. 코드구현력 기르기/18. 층간소음.cpp
@@ -0,0 +1,27 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	int n, m;
+	int max = -1;
+	int second = 0;
+	scanf("%d %d", &n, &m);
+
+	int sound;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &sound);
+		if (sound <= m) {
+			second = 0;
+		}
+		else {
+			second++;
+			if (max < second)
+				max = second;
+		}
+	}
+
+	printf("%d\n", max);
+	return 0;
+}

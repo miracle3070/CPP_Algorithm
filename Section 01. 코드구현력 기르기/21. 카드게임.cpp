11d2a23d0a4194f982062e8bb2cf52921392f259
miracle3070 <miracle3070@naver.com>
2023-06-30 17:03:32 +0900

    [sec01] 21. 카드게임

---
+++ Section 01. 코드구현력 기르기/21. 카드게임.cpp
@@ -0,0 +1,52 @@
+﻿#pragma warning(disable:4996)
+#define MAX_LEN 10
+#include <cstdio>
+#include <cstring>
+
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	int A[MAX_LEN];
+	int B[MAX_LEN];
+
+	for (int i = 0; i < MAX_LEN; i++) {
+		scanf("%d", &A[i]);
+	}
+
+	for (int i = 0; i < MAX_LEN; i++) {
+		scanf("%d", &B[i]);
+	}
+
+	int pointA = 0;
+	int pointB = 0;
+	bool isDraw = true;
+	char lastWinner;
+
+	for (int i = 0; i < MAX_LEN; i++) {
+		if (A[i] == B[i]) {
+			pointA++;
+			pointB++;
+		}
+		else if (A[i] > B[i]) {
+			pointA += 3;
+			lastWinner = 'A';
+			isDraw = false;
+		}
+		else {
+			pointB += 3;
+			lastWinner = 'B';
+			isDraw = false;
+		}
+	}
+
+	printf("%d %d\n", pointA, pointB);
+	if (isDraw)
+		printf("D\n");
+	else if (pointA > pointB)
+		printf("A\n");
+	else if (pointA < pointB)
+		printf("B\n");
+	else
+		printf("%c\n", lastWinner);
+
+	return 0;
+}

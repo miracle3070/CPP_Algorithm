ac6319397574a34ee511030e07da57ed59758a48
miracle3070 <miracle3070@naver.com>
2023-07-06 23:23:42 +0900

    [sec01] 22. 온도의 최대값

---
+++ Section 01. 코드구현력 기르기/22. 온도의 최대값.cpp
@@ -0,0 +1,32 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+
+int day[100000];
+int max = -(int)(1e9);
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	int n, k;
+	scanf("%d %d", &n, &k);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &day[i]);
+	}
+
+	int temp = 0;
+	int i = 0;
+	for (; i < k; i++) {
+		temp += day[i];
+	}
+	max = temp;
+	
+	for (; i < n; i++) {
+		temp -= day[i - k];
+		temp += day[i];
+		if (max < temp)
+			max = temp;
+	}
+
+	printf("%d\n", max);
+	return 0;
+}

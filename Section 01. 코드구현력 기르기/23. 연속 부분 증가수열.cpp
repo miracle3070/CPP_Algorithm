ed7e4b95c61fdfcd9b14a18da2dd2204151dcb16
miracle3070 <miracle3070@naver.com>
2023-07-07 16:41:14 +0900

    [sec01] 23. 연속 부분 증가수열

---
+++ Section 01. 코드구현력 기르기/23. 연속 부분 증가수열.cpp
@@ -0,0 +1,34 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+int main(void) {
+//	freopen("input.txt", "rt", stdin);
+	int n;
+	int max = 0;
+	scanf("%d", &n);
+
+	vector<int> num(n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &num[i]);
+	}
+
+	int len = 0;
+	int target = 0;
+	for (int i = 0; i < n; i++) {
+		if (target <= num[i]) {
+			target = num[i];
+			if (max < ++len)
+				max = len;
+		}
+		else {
+			target = num[i];
+			len = 1;
+		}
+	}
+
+	printf("%d\n", max);
+	return 0;
+}

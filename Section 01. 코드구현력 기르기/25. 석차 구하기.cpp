88aeed5891b3cb4e00da4901fadbd7bcd40c96a1
miracle3070 <miracle3070@naver.com>
2023-07-08 14:44:55 +0900

    [sec01] 25. 석차 구하기

---
+++ Section 01. 코드구현력 기르기/25. 석차 구하기.cpp
@@ -0,0 +1,40 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	int n;
+	scanf("%d", &n);
+	vector<int> score(n);
+	vector<int> memory(n);
+	unordered_map<int, int> scoreMap;
+
+	for (int i = 0; i < n; i++) {
+		int input;
+		scanf("%d", &input);
+		score[i] = input;
+		memory[i] = input;
+	}
+
+	sort(score.rbegin(), score.rend());
+	int pre_score = 0;
+	for (int i = 0; i < n; i++) {
+		if (pre_score == score[i])
+			continue;
+		
+		scoreMap.insert(make_pair(score[i], i + 1));
+		pre_score = score[i];
+	}
+
+	for (int score : memory) {
+		printf("%d ", scoreMap.at(score));
+	}
+
+	printf("\n");
+	return 0;
+}

9053db6ead13fc6d25ef21acc1bcd79e267cf1f4
miracle3070 <miracle3070@naver.com>
2023-07-08 13:51:50 +0900

    [sec01] 24. Jolly Jumpers

---
+++ Section 01. 코드구현력 기르기/24. Jolly Jumpers.cpp
@@ -0,0 +1,44 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+int main() {
+//	freopen("input.txt", "rt", stdin);
+	int n;
+	scanf("%d", &n);
+
+	vector<int> num(n);
+	vector<bool> check(n);
+	bool result = true;
+	int temp;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &num[i]);
+	}
+
+	for (int i = 0; i < n - 1; i++) {
+		temp = num[i] - num[i + 1];
+		if (temp < 0)
+			temp = -temp;
+
+		if (temp < 1 || n <= temp) {
+			result = false;
+			break;
+		}
+
+		if (check[temp] == true) {
+			result = false;
+			break;
+		}
+
+		check[temp] = true;
+	}
+
+	if (result)
+		printf("YES\n");
+	else
+		printf("NO\n");
+
+	return 0;
+}

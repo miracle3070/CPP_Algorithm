f79c0e0d1f2e7898e05960c22c85ea223fbf0b69
miracle3070 <miracle3070@naver.com>
2023-08-02 17:54:48 +0900

    [sec01] 26. 말아톤

---
+++ Section 01. 코드구현력 기르기/26. 말아톤.cpp
@@ -0,0 +1,29 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+//	freopen("input.txt", "rt", stdin);
+	int n;
+	scanf("%d", &n);
+	vector<int> players(n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &players[i]);
+	}
+
+	for (int i = 0; i < n; i++) {
+		int rank = i;
+		for (int k = i; k >= 0; k--) {
+			if (players[i] > players[k])
+				rank--;
+		}
+		printf("%d ", rank + 1);
+	}
+	printf("\n");
+
+	return 0;
+}
